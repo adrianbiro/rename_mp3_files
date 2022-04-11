28bdc3f01d8b65862ab24fc2d8811a4cd7719cfb
Adrián Bíro <biroadrian@protonmail.com>
2022-04-11 15:17:15 +0200

    search for mp3 files

---
+++ search_for_mp3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import fnmatch
+import os
+
+Path = os.getcwd()
+pattern = '*.mp3'
+
+"""Check where are .mp3 files store recursively from the current working directory."""
+dirs_with_mp3 = []
+for root, dirs, files in os.walk(Path):
+    for filename in fnmatch.filter(files, pattern):
+        #dirs_with_mp3.append(os.path.join(root, filename))
+        if (root not in dirs_with_mp3):
+            dirs_with_mp3.append(os.path.join(root))
+"""Print locations out."""
+for i in dirs_with_mp3:
+    print(i)
