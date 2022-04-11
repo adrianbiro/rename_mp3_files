7f6a268caec449e318a263bf16dcc377b66cb608
Adrián Bíro <biroadrian@protonmail.com>
2022-04-11 19:58:48 +0200

    small fixes

--- rename_metadata_souostrovi_gulag.py
+++ rename_metadata_souostrovi_gulag.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+from mutagen.easyid3 import EasyID3
 
 os.chdir('/home/adrian/Music/Alexander Solženicyn/Alexandr Solženicyn - Souostroví Gulag/')
 for i in os.listdir():
@@ -8,12 +9,15 @@ for i in os.listdir():
     file_name, file_extension = os.path.splitext(i)
     num = "".join(file_name.split()[:1])
     num = num.replace(".", "")
-    title = "_".join(file_name.split()[1:]) 
+    title = "_".join(file_name.split()[1:])
 
     new_name = f'{num}-{title}{file_extension}'
-    
+
     os.rename(i, new_name)
 
+    audio = EasyID3(i)  # edit metadata
+    audio["title"] = file_name
+    audio.save()
 
 """
 052. Souostroví Gulag.mp3
